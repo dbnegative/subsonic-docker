6abfd51c91b9c712bee89e1479d42b974a5a3eb9
Jason Witting <jason.witting@MacBook-Pro-4.local>
2016-07-20 16:56:30 +0100

    Added  sudo

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ set -e
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/subsonic
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"subsonic","image":"us.gcr.io/kubernetes/subsonic:'"$CIRCLE_SHA1"'"}]}}}}'
+sudo kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"subsonic","image":"us.gcr.io/kubernetes/subsonic:'"$CIRCLE_SHA1"'"}]}}}}'
