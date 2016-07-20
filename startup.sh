2d83d0c966eaef9817ef183d7e9c398fa45b6363
Jason Witting <jason.witting@MacBook-Pro-4.local>
2016-07-20 17:25:31 +0100

    ameded startup script to use sleep instead of read

--- startup.sh
+++ startup.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
-
-# USE the trap if you need to also do manual cleanup after the service is stopped,
-#     or need to start multiple services in the one container
-trap "echo TRAPed signal" HUP INT QUIT TERM
-
 # start service in background here
 /var/subsonic/standalone/subsonic.sh
-
-sleep 5
-
-read
-
+sleep 9999
 echo "exited $0"
